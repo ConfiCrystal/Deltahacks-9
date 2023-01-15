8adca91082e5bac7e0b1affa02a73c12f5f8edea
IsopodOverseer-Dason <dasonxywang@gmail.com>
2023-01-15 10:49:10 -0500

    Final Product

--- service-worker.js
+++ service-worker.js
@@ -22,8 +22,8 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => {
-        return cache.addAll(precacheResources);
         document.getElementById("#installicon").style.display = "none";
+        return cache.addAll(precacheResources);
       })
   );
 });
